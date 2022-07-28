afd019fab8715f79960b524f2daed7d9e5ed9b92
unknown <tungdao923@gmail.com>
2022-07-28 16:38:55 +0700

    stop watch

---
+++ src/StopWatch.java
@@ -0,0 +1,26 @@
+import java.time.LocalTime;
+
+public class StopWatch {
+    private long   startTime, endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void stop(){
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return endTime - startTime;
+    }
+}
